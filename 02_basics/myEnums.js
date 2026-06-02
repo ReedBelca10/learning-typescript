dfcc85fef287e201c1fb1e7d24ad8650320da1c1
ReedBelca <reedbelca55@gmail.com>
2026-06-02 16:27:44 +0000

    Enums

---
+++ 02_basics/myEnums.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var hcSeat = "AISLE" /* SeatChoice.AISLE */;
---
+++ 02_basics/myEnums.ts
@@ -0,0 +1,11 @@
+// Enums are a way to define a set of named constants. They can be numeric or string values. Enums are useful when you have a fixed set of values that you want to represent as a type.
+const enum SeatChoice {
+    AISLE = "AISLE",
+    MIDDLE = "MIDDLE",
+    WINDOW = "WINDOW"
+}
+
+// Enums can be used to define a set of named constants. They can be numeric or string values. Enums are useful when you have a fixed set of values that you want to represent as a type.
+const hcSeat = SeatChoice.MIDDLE
+
+export { }
