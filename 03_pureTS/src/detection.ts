f7e3c8726c37480d6c73b4bc8a098b7ba45e0bfb
ReedBelca <reedbelca55@gmail.com>
2026-07-19 18:30:02 +0000

    Descriminated Union and Exhaustiveness Checking with Never

--- 03_pureTS/src/detection.ts
+++ 03_pureTS/src/detection.ts
@@ -70,3 +70,46 @@ function getFood(pet: Fish | Bird) {
         return "bird food";
     }
 }
+
+interface Circle {
+    kind: "circle";
+    radius: number;
+}
+
+interface Square {
+    kind: "square";
+    sideLength: number;
+}
+
+interface Rectangle {
+    kind: "rectangle";
+    length: number;
+    width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function   getTrueShape(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    } else if (shape.kind === "square") {
+        return shape.sideLength ** 2;
+    } else {
+        return shape.length * shape.width;
+    }
+}
+
+//Exhaustive checking with never type
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.sideLength ** 2;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _exhaustiveCheck: never = shape;
+            return _exhaustiveCheck;
+    }
+}
